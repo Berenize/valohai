44f1c9856b4e581853996d40edd39d0ad75e5e11
Berenize <38691227+Berenize@users.noreply.github.com>
2018-04-24 15:54:02 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import numpy
+import pandas
+from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
